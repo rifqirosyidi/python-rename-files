4a3fd950c660e075f9c71240e1f8a5dc4fd1f82d
Rifqi <rief.rosyidi@gmail.com>
2019-06-25 09:12:51 +0700

    image is now renamed

--- rename.py
+++ rename.py
@@ -1,13 +1,17 @@
 import os
 def rename_file():
-	# Get file names from folder
+	# Get file names to be renamed from folder
 	file_list = os.listdir("/home/rief/Documents/Coding - Python/Renaming Code/img")
-	print(file_list)
+	original_path = os.getcwd()
 
+	# Change directory to the image Location
+	os.chdir("{}/img".format(os.getcwd()))
 
 	# For each file, rename filename
 	for file_name in file_list:
-		os.rename(file_name, file_name.translate(None, "0123456789"))
+		new_file_name = file_name.translate(str.maketrans("", "", "1234567890"))
+		os.rename(file_name, new_file_name)
 
+	os.chdir(original_path)
 
 rename_file()
